eb9639080a4484b06ec014e8ed6fabc3501e3f3a
gautam <gautamjsuvarna@gmail.com>
2021-10-31 18:21:43 +0530

    v1 done

---
+++ mysql_connector.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+
+#code for connecting sql databse with python for fetching and analysing it
+mydb = mysql.connector.connect(host="localhost",user='gautam',passwd='gautam',database='test') #auth details
+mycursor= mydb.cursor()    #just added variable to make code readable.
+mycursor.execute("select * from student") #you can copy this sent and add your own query.
+result =mycursor.fetchall()  #fetch funtion has been used there are many more option. refer documentation.
+
+#loop for printing out data . result can be changed to any varaible
+for i in result:
+    print(i)
---
+++ signin.py
@@ -0,0 +1,16 @@
+
+print("Hello welcome to One Bloodline:-")
+#signin steps
+#function main programming
+def signin():
+    global result
+    username = input("Enter Username/E-mail/Mobile-number: ")
+    password = input("Enter New Password: ")
+    result = [username,password]
+
+#execution of functions
+signin()
+print(result)
+print("signin process is  working fine.(only for debugging purpose)")
+#for fetching data in sql query
+print(result[1])
