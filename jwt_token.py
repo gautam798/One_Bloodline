6ebb4f092f5a3b8341f210c49c3de970612dd057
gautam798 <gautamjsuvarna@gmail.com>
2021-10-31 22:24:29 +0530

    authorization work

---
+++ jwt_token.py
@@ -0,0 +1,15 @@
+#authotication mechanish
+import jwt
+#shared secret is confidential dont disclose it any where
+shared_secret=""
+
+#working for athurization using jwt token
+key_data= shared_secret
+payload_data ={
+"email":"test@gmail.com",
+"name":"test",
+"Id":1
+}
+access_token = jwt.encode(payload=payload_data,key=key_data)
+
+print(access_token)
